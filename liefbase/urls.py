3f5e7d6f4bcdcbf612796f48a89c6e1cafd5fac9
Konrad <konrad_here@hotmail.com>
2017-08-27 17:09:45 -0700

    add jwt token authentication

--- liefbase/urls.py
+++ liefbase/urls.py
@@ -1,7 +1,14 @@
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
+
 from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('web.urls', namespace="web")),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
 ]
